ab6d51469dabf0d53cac4885b5997309155655cc
turncoda <144861042+turncoda@users.noreply.github.com>
2026-07-19 23:46:21 -0400

    Parse map file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+use std::fs;
+use shalrath::parser::repr::parse_map;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    assert_eq!(args.len(), 2, "please pass in a map file");
+
+    let map_contents = fs::read_to_string(&args[1]).expect("failed to read map file");
+
+    let (_, ast) = parse_map(&map_contents).expect("failed to parse map file");
+
+    println!("{:#?}", ast);
 }
