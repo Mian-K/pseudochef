eb07eddecf70c151a8cf6ae8038c6f471d2aa1b7
turncoda <144861042+turncoda@users.noreply.github.com>
2026-07-20 21:18:18 -0400

    Import repak crate and write empty pak file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use shalrath::parser::repr::parse_map;
 use std::env;
 use std::fs;
-use shalrath::parser::repr::parse_map;
+use std::fs::File;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,6 +11,20 @@ fn main() {
     let map_contents = fs::read_to_string(&args[1]).expect("failed to read map file");
 
     let (_, ast) = parse_map(&map_contents).expect("failed to parse map file");
+    _ = ast;
 
-    println!("{:#?}", ast);
+    //println!("{:#?}", ast);
+
+    let output = "out.pak";
+    let mut pak = repak::PakBuilder::new().writer(
+        File::create(&output).unwrap(),
+        repak::Version::V11,
+        "../../../pseudoregalia/Content/".to_string(),
+        None,
+    );
+    // TODO generate obj files
+    // TODO include prefab asset files
+    let umap = "".to_string();
+    pak.write_file("Mods/Maps/MyMap.umap", true, &umap).unwrap();
+    pak.write_index().unwrap();
 }
