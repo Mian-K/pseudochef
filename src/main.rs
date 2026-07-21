ab78ff7daea82989d51d93b4261c279f0fea99cf
turncoda <144861042+turncoda@users.noreply.github.com>
2026-07-21 17:42:41 -0400

    Stub in unreal_asset parse, no-op, and re-serialize

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,44 @@
 use shalrath::parser::repr::parse_map;
-use std::env;
-use std::fs;
-use std::fs::File;
+use std::fs::{File, read_to_string};
+
+const MISE_UMAP: &[u8] = include_bytes!("mise.umap");
+const MISE_UEXP: &[u8] = include_bytes!("mise.uexp");
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = std::env::args().collect();
 
-    assert_eq!(args.len(), 2, "please pass in a map file");
+    assert_eq!(args.len(), 3, "usage: pseudochef IN_MAP OUT_PAK");
 
-    let map_contents = fs::read_to_string(&args[1]).expect("failed to read map file");
+    let map_contents = read_to_string(&args[1]).expect("failed to read map file");
 
     let (_, ast) = parse_map(&map_contents).expect("failed to parse map file");
     _ = ast;
 
-    //println!("{:#?}", ast);
-
-    let output = "out.pak";
     let mut pak = repak::PakBuilder::new().writer(
-        File::create(&output).unwrap(),
+        File::create(&args[2]).unwrap(),
         repak::Version::V11,
         "../../../pseudoregalia/Content/".to_string(),
         None,
     );
-    // TODO generate obj files
-    // TODO include prefab asset files
-    let umap = "".to_string();
-    pak.write_file("Mods/Maps/MyMap.umap", true, &umap).unwrap();
+
+    let umap = unreal_asset::Asset::new(
+        std::io::Cursor::new(MISE_UMAP),
+        Some(std::io::Cursor::new(MISE_UEXP)),
+        unreal_asset::engine_version::EngineVersion::VER_UE5_1,
+        None,
+    )
+    .expect("failed to parse umap");
+
+    // TODO generate obj files and write them to the umap and the pak
+
+    let mut final_umap = std::io::Cursor::new(vec![]);
+    let mut final_uexp = std::io::Cursor::new(vec![]);
+    umap.write_data(&mut final_umap, Some(&mut final_uexp))
+        .expect("failed to serialize umap");
+
+    pak.write_file("Mods/Maps/mise.umap", true, final_umap.get_ref())
+        .expect("failed to write umap to pak");
+    pak.write_file("Mods/Maps/mise.uexp", true, final_uexp.get_ref())
+        .expect("failed to write uexp to pak");
     pak.write_index().unwrap();
 }
---
+++ test.bat
@@ -0,0 +1,1 @@
+call "C:\Program Files (x86)\Steam\steamapps\common\Pseudoregalia\pseudoregalia\Binaries\Win64\pseudoregalia-Win64-Shipping.exe" "/Game/Mods/Maps/mise" -upgrades=attack,light
