4ed932faaa893f6b461312f81b6bab25c98f35c0
turncoda <144861042+turncoda@users.noreply.github.com>
2026-07-23 00:51:46 -0400

    Steal basic brush-to-mesh code from pseudomaker

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+use glam::{Mat3, Quat, Vec3};
+use itertools::Itertools;
 use pseudocooker::{Corner, Face};
 use shalrath::parser::repr::parse_map;
 use std::fs::{File, read_to_string};
 use unreal_asset::exports::ExportBaseTrait;
 use unreal_asset::exports::ExportNormalTrait;
+
+const PI: f32 = std::f32::consts::PI;
 
 const MISE_UMAP: &[u8] = include_bytes!("mise.umap");
 const MISE_UEXP: &[u8] = include_bytes!("mise.uexp");
@@ -58,6 +62,136 @@ fn pyramid_mesh() -> pseudocooker::MeshInput {
     }
 }
 
+fn vec3(p: shalrath::repr::Point) -> Vec3 {
+    Vec3::new(p.x, p.y, p.z)
+}
+fn angle_between(a: &Vec3, b: &Vec3) -> f32 {
+    trunc_with_precision(a.dot(*b) / (a.length() * b.length()), 1e-5).acos()
+}
+fn trunc_with_precision(f: f32, precision: f32) -> f32 {
+    (f / precision).trunc() * precision
+}
+fn approx_eq(a: f32, b: f32, epsilon: f32) -> bool {
+    (a - b).abs() <= epsilon
+}
+fn calculate_normal(plane: shalrath::repr::TrianglePlane) -> Vec3 {
+    let p0 = vec3(plane.v0);
+    let p1 = vec3(plane.v1);
+    let p2 = vec3(plane.v2);
+    let a = p2 - p0;
+    let b = p1 - p0;
+    a.cross(b)
+}
+fn flip_y(point: &mut shalrath::repr::Point) {
+    point.y = -point.y;
+}
+fn mirror_xz(brush: &shalrath::repr::Brush) -> shalrath::repr::Brush {
+    let mut planes = vec![];
+    for plane in &brush.0 {
+        let mut mirrored = plane.clone();
+        flip_y(&mut mirrored.plane.v0);
+        flip_y(&mut mirrored.plane.v1);
+        flip_y(&mut mirrored.plane.v2);
+        planes.push(mirrored);
+    }
+    shalrath::repr::Brush::new(planes)
+}
+fn convert_to_mesh(brush: &shalrath::repr::Brush) -> pseudocooker::MeshInput {
+    // TrenchBroom uses a right-handed coordinate system with +z pointing up.
+    // Unreal uses a left-handed coordinate system with +z pointing up.
+    // So, we mirror across the xz-plane when converting.
+    let brush = mirror_xz(&brush);
+
+    let mut vertices = vec![vec![]; brush.0.len()];
+    let mut normals = vec![];
+    for plane in &brush.0 {
+        normals.push(calculate_normal(plane.plane));
+    }
+    let planes_and_normals: Vec<_> = brush
+        .0
+        .iter()
+        .enumerate()
+        .map(|(i, plane)| (i, plane.plane, calculate_normal(plane.plane)))
+        .collect();
+    for c in planes_and_normals.iter().combinations(3) {
+        // TODO generalize this to handle planes at arbitrary angles
+        let (i0, p0, n0) = c[0];
+        let (i1, p1, n1) = c[1];
+        let (i2, p2, n2) = c[2];
+        let a0 = angle_between(n0, n1).abs();
+        let a1 = angle_between(n1, n2).abs();
+        let a2 = angle_between(n2, n0).abs();
+        let b0 = approx_eq(a0, 0.5 * PI, 1e-2);
+        let b1 = approx_eq(a1, 0.5 * PI, 1e-2);
+        let b2 = approx_eq(a2, 0.5 * PI, 1e-2);
+        if b0 && b1 && b2 {
+            let x0 = vec3(p0.v0);
+            let x1 = vec3(p1.v0);
+            let x2 = vec3(p2.v0);
+            let u0 = n0.normalize();
+            let u1 = n1.normalize();
+            let u2 = n2.normalize();
+            let det = Mat3::from_cols(u0, u1, u2).determinant();
+            let c0 = x0.dot(u0) * u1.cross(u2);
+            let c1 = x1.dot(u1) * u2.cross(u0);
+            let c2 = x2.dot(u2) * u0.cross(u1);
+            let intersection = det.powi(-1) * (c0 + c1 + c2);
+            vertices[*i0].push(intersection);
+            vertices[*i1].push(intersection);
+            vertices[*i2].push(intersection);
+        }
+    }
+
+    let mut positions = vec![];
+    let mut faces = vec![];
+    for (normal, face_vertices) in normals.iter().zip(vertices.iter()) {
+        let base = positions.len();
+        let quat = Quat::from_rotation_arc(normal.clone(), Vec3::Z);
+        let mut polar_angles = vec![];
+        for (i, vertex) in face_vertices.iter().enumerate() {
+            positions.push([vertex.x as f64, vertex.y as f64, vertex.z as f64]);
+            let rv = quat * vertex;
+            let theta = ordered_float::OrderedFloat(rv.y.atan2(rv.x));
+            polar_angles.push((theta, i));
+        }
+        polar_angles.sort();
+        let mut tris = vec![];
+        for i in 1..polar_angles.len() - 1 {
+            let a = polar_angles[0].1;
+            let b = polar_angles[i].1;
+            let c = polar_angles[i + 1].1;
+            // TODO unhardcode material_index; would need to pass in whole entity, not just brush
+            let f = pseudocooker::Face {
+                material_index: 0,
+                corners: vec![
+                    pseudocooker::Corner::new(base + a),
+                    pseudocooker::Corner::new(base + b),
+                    pseudocooker::Corner::new(base + c),
+                ],
+            };
+            tris.push(f);
+        }
+        faces.extend(tris);
+    }
+
+    pseudocooker::MeshInput {
+        positions,
+        uvs: Vec::new(),
+        normals: Vec::new(),
+        faces,
+        material_names: vec![],
+    }
+    // TODO break up each face for more even vertex lighting
+}
+
+fn default_opts() -> pseudocooker::CookOptions {
+    pseudocooker::CookOptions {
+        body_setup_guid: None,
+        lighting_guid: None,
+        package_guid: None,
+    }
+}
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
 
@@ -66,7 +200,6 @@ fn main() {
     let map_contents = read_to_string(&args[1]).expect("failed to read map file");
 
     let (_, ast) = parse_map(&map_contents).expect("failed to parse map file");
-    _ = ast;
 
     let mut pak = repak::PakBuilder::new().writer(
         File::create(&args[2]).unwrap(),
@@ -74,6 +207,23 @@ fn main() {
         "../../../pseudoregalia/Content/".to_string(),
         None,
     );
+
+    for (i, ent) in ast.0.iter().enumerate() {
+        for brush in &ent.brushes.0 {
+            let mesh = convert_to_mesh(&brush);
+            let name = format!("tb{}", i);
+            let cooked = pseudocooker::cook(&mesh, &name, false, 4.0, &default_opts());
+            pak.write_file(
+                &format!("Mods/Maps/slop/{}.uasset", name),
+                true,
+                &cooked.uasset,
+            )
+            .expect("failed to write uasset to pak");
+            pak.write_file(&format!("Mods/Maps/slop/{}.uexp", name), true, &cooked.uexp)
+                .expect("failed to write uasset to pak");
+            println!("cooked {}", name);
+        }
+    }
 
     let mut umap = unreal_asset::Asset::new(
         std::io::Cursor::new(MISE_UMAP),
@@ -89,7 +239,7 @@ fn main() {
             .get_import(unreal_asset::types::PackageIndex::new(-39))
             .unwrap();
         let mut new_import = import.clone();
-        new_import.object_name = umap.add_fname("/Game/Mods/Maps/slop/cookout");
+        new_import.object_name = umap.add_fname("/Game/Mods/Maps/slop/tb0");
         umap.add_import(new_import);
     }
 
@@ -100,7 +250,7 @@ fn main() {
             .unwrap();
         let mut new_import = import.clone();
         new_import.outer_index = unreal_asset::types::PackageIndex::new(-55);
-        new_import.object_name = umap.add_fname("cookout");
+        new_import.object_name = umap.add_fname("tb0");
         umap.add_import(new_import);
     }
 
@@ -163,7 +313,7 @@ fn main() {
             .get_export(unreal_asset::types::PackageIndex::new(i + 1))
             .unwrap();
         let object_name = &export.get_base_export().object_name;
-        println!("{}: {}", i + 1, fname_to_str(object_name));
+        //println!("{}: {}", i + 1, fname_to_str(object_name));
         let props = export.get_normal_export().unwrap().properties.clone();
         for prop in props {
             if let unreal_asset::properties::Property::ObjectProperty(obj_prop) = prop {
@@ -172,25 +322,12 @@ fn main() {
         }
     }
 
-    let opt = pseudocooker::CookOptions {
-        body_setup_guid: None,
-        lighting_guid: None,
-        package_guid: None,
-    };
-    let cooked = pseudocooker::cook(&pyramid_mesh(), "cookout", true, 100.0, &opt);
-    pak.write_file("Mods/Maps/slop/cookout.uasset", true, &cooked.uasset).expect("failed to write uasset to pak");
-    pak.write_file("Mods/Maps/slop/cookout.uexp", true, &cooked.uexp).expect("failed to write uasset to pak");
-
     // TODO generate obj files and write them to the umap and the pak
 
     let mut final_umap = std::io::Cursor::new(vec![]);
     let mut final_uexp = std::io::Cursor::new(vec![]);
     umap.write_data(&mut final_umap, Some(&mut final_uexp))
         .expect("failed to serialize umap");
-
-    // TODO temporary for debugging
-    std::fs::write("slop.umap", final_umap.get_ref()).expect("failed to write umap file");
-    std::fs::write("slop.uexp", final_uexp.get_ref()).expect("failed to write uexp file");
 
     // TODO also rename mise export
     pak.write_file("Mods/Maps/slop.umap", true, final_umap.get_ref())
