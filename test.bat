105e8ed0ec22dae42445021ae8a9f6148b5b6c75
turncoda <144861042+turncoda@users.noreply.github.com>
2026-07-28 01:22:28 -0400

    Remove test.bat

--- test.bat
+++
@@ -1,1 +0,0 @@
-call "C:\Program Files (x86)\Steam\steamapps\common\Pseudoregalia\pseudoregalia\Binaries\Win64\pseudoregalia-Win64-Shipping.exe" "/Game/Mods/Maps/slop" -upgrades=attack,light -spawn=spawn1
